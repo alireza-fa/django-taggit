9a2cb08e89d1444e13bd63206b444edbc3eba580
alireza-fa <alirezafeyze44@gmail.com>
2024-04-10 22:37:21 +0330

    startapp taggit

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local apps
+    'taggit.apps.TaggitConfig',
+    # Third party apps
 ]
 
 MIDDLEWARE = [
